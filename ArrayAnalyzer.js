23e47b4cb61918bc1e6d32573429f8fd888f722f
Chan Nirak Choun <nirakchoun@gmail.com>
2024-06-25 22:41:27 -0500

    Combine into one loop

--- ArrayAnalyzer.js
+++ ArrayAnalyzer.js
@@ -1,42 +1,38 @@
 function analyzeArray(arr) {
+    if (arr.length === 0) {
+        console.log("The array is empty");
+        return;
+    }
+
     let max = arr[0];
     let min = arr[0];
-    let avg = 0;
     let sum = 0;
     let evenArr = [];
     
-    // Find Maximum
     for (let i = 0; i < arr.length; i++) {
-        if (max < arr[i]) {
+        // Find Maximum
+        if (arr[i] > max) {
             max = arr[i];
         }
-    }
-    
-    // Find Minimum
-    for (let i = 0; i < arr.length; i++) {
-        if (min > arr[i]) {
+        
+        // Find Minimum
+        if (arr[i] < min) {
             min = arr[i];
         }
-    }
-    
-    // Find Average
-    for (let i = 0; i < arr.length; i++) {
+        
         sum += arr[i];
-        avg = sum / arr.length;
-    }
-    
-    // Find Even Array
-    for (let i = 0; i < arr.length; i++) {
+
+        // Find Even Numbers
         if (arr[i] % 2 === 0) {
             evenArr.push(arr[i]);
         }
     }
-    
+
+    // Find Average
+    let avg = sum / arr.length;
+
     console.log(`The maximum of the array is ${max}`);
     console.log(`The minimum of the array is ${min}`);
     console.log(`The average of the array is ${avg}`);
-    console.log(`The even numbers of the array is ${evenArr}`);
-    
+    console.log(`The even numbers of the array are ${evenArr}`);
 }
- 
- 
